9121de3496d5066d20dc273b77d7f1c7e69ff752
Jillian Maher <40473408+jillianmaher@users.noreply.github.com>
2020-10-07 11:01:37 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,19 +6,21 @@ public class Main
 	public static void main( String[] args )
 	{
 		Dog d = new Dog( "rover" );
-		System.out.println( d.getPeopleAge( 7 ) );  //will output 7
+		System.out.println( d.getDogYears( 7 ) );  //will output 7
 		d.increaseAgeByOne();
 		d.increaseAgeByOne();     //age should be 3 after this call
-		System.out.println( d.getPeopleAge( 11 ) );  //will output 33
+		System.out.println( d.getDogYears( 11 ) );  //will output 33
  		System.out.println( d.getName() );	  //will output rover
 // 		d.increaseAgeByOne();
 // 		d.increaseAgeByOne();
 // 		d.increaseAgeByOne();
 // 		d.increaseAgeByOne();    //age should be 7 after this call
-// 		System.out.println( d.getPeopleAge( 8 ) );  //will output 56
+// 		System.out.println( d.getDogYears( 8 ) );  //will output 56
+
+//		Add more of your own test cases
 // 		Dog g = new Dog( "bob", 3 );
 // 		System.out.println( g.getName() );
-// 		System.out.println( g.getPeopleAge( 5 ) );
+// 		System.out.println( g.getDogYears( 5 ) );
 		
 	}
 }
