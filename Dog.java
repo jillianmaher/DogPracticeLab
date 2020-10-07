fa4b56c09d0c1687d200df71ef052355229181a2
Jillian Maher <40473408+jillianmaher@users.noreply.github.com>
2020-10-07 10:34:59 -0400

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,42 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+public class Dog
+{
+  private int age;
+  private String name;
+	
+  public Dog( String n )  
+  {
+    age = 1;
+    name = n;
+  }
+  
+	public Dog( String n, int a )  
+	{
+ 	 //fill in this constructor
+	}
+  
+  public void increaseAgeByOne()
+  {
+  	 //fill in this method
+  }
+  
+  public int getPeopleAge( int val )
+  {
+  	 //fill in this method
+  	 return 0;
+  }
+	
+  public int getAge()    {
+    return age;
+  }
+	
+  public String getName()    {
+    return name;
+  }
+	
+  public String toString()    {
+    return "Dog - " + name + " " + age;
+  }
+}
---
+++ DogRunner.java
@@ -0,0 +1,11 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+public class DogRunner
+{
+	public static void main( String[] args )
+	{
+		//add test cases	
+		//use the examples on the lab handout			
+	}
+}
